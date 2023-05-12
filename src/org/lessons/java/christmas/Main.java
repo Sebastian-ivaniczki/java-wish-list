c0d2cea2d77a4654d159950d9e58e494fe755982
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-05-12 11:54:37 +0200

    finish

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,7 +1,32 @@
 package org.lessons.java.christmas;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-	
+		Map<Integer, String> wishList = new HashMap<>();
+		
+		Scanner sc = new Scanner(System.in);
+		boolean isGo = true;
+		Integer count = 0;
+		while(isGo) {
+			System.out.println("per aggiungere qualcosa alla tua wish list digita 1 per terminare digita tua");
+			int val = sc.nextInt();
+			
+			if(val == 1) {
+				String t = sc.nextLine();
+				System.out.println("aggiungi un elemento alla tua whish list");
+				String whish =  sc.nextLine();
+				wishList.put(count++, whish);
+				System.out.println(wishList);
+			} else if(val == 2) {
+				System.out.println("grazie e ciao");
+				isGo = false;
+			}
+		}
+				
+		System.out.println("ecco la tua lista desideri: " + wishList);	
 	}
 }
